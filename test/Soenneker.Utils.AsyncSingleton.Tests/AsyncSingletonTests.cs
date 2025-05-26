d742be447ee63fd1e233fb444ee5d22c8e5b4748
Jake Soenneker <jake@soeneker.com>
2025-05-26 23:38:43 +0000

    FluentAssertions -> AwesomeAssertions namespace

--- test/Soenneker.Utils.AsyncSingleton.Tests/AsyncSingletonTests.cs
+++ test/Soenneker.Utils.AsyncSingleton.Tests/AsyncSingletonTests.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Xunit;
 
 namespace Soenneker.Utils.AsyncSingleton.Tests;
--- test/Soenneker.Utils.AsyncSingleton.Tests/ObjectInitializationTests.cs
+++ test/Soenneker.Utils.AsyncSingleton.Tests/ObjectInitializationTests.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Xunit;
 
 namespace Soenneker.Utils.AsyncSingleton.Tests;
